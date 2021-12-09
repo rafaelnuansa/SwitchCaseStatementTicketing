e76c9cdda5b432fa73fa27a2c632bd70929701c4
Rafael Nuansa <iandrewnew@gmail.com>
2021-12-09 11:56:18 +0700

    first commit

---
+++ src/ticketingStatus.java
@@ -0,0 +1,27 @@
+
+public class ticketingStatus {
+	public static void main(String[] args) {
+		// Penentuan nilai dalam Status Ticket
+		// Rafael Nuansa Ramadhon
+		// Universitas Djuanda 
+		// Disini saya menuliskan 1 contoh customer baru saja mengirimkan ticket baru / open ticket dan otomatis statusnya akan diprogress
+	    int status_ticket = 1;
+	    switch (status_ticket) {
+	      case 1:
+	        System.out.println("In Progress");
+	        break;
+	      case 2:
+		//   Proses Answered yaitu sudah dijawab oleh pihak Staff/pengelola dari aplikasi Ticketing 
+	        System.out.println("Answered");
+	        break;
+	      case 3:
+		//   Proses dimana sudah dijawab kembali oleh customer 
+	        System.out.println("Reply by Customers");
+	        break;
+	      case 4:
+		//   Penutupan ticket bahwa ticket sudah ditutup dan troublesolving sudah complete 
+	        System.out.println("Closed Ticket");
+	        break;
+	    }
+	  }
+}
